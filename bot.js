03db5ad65639de2d93100fbd7157e84220d93c7c
charuri <charlina.hung@gmail.com>
2017-08-22 20:07:41 -0500

    added automatic flower picking functinality

--- bot.js
+++ bot.js
@@ -32,23 +32,34 @@ function createDiscordBot() {
         autorun: true,
         token: discordToken
     })
-    console.log("-> discord bot initialized");  
+    console.log("-> discord bot initialized");
     // start bot
     discordBot.on('ready', function (event) {
         console.log('-> logged in as %s - %s\n', discordBot.username, discordBot.id);
-        
+
         // begin shitpost loop
         console.log('starting to fish');
         getFishy();
     });
-    
+
     discordBot.on('message', function(user, userID, channelID, message, event) {
         // do nothing...
+        console.log("user ", user);
+        console.log("userID ", userID);
+        console.log("channelID ", channelID);
+        console.log("message ", message);
+        console.log("event mentions ", event.d.mentions);
+        console.log("event embeds" , event.d.embeds);
+        console.log("\n");
+
+        if (message.startsWith(".plant") || message.startsWith("5 random")) {
+            pickFlower(channelID);
+        }
     });
 }
 
 function getRandomPause() {
-    return Math.min((Math.abs(distribution.ppf(Math.random())) + 1) * 30000, 1500000) 
+    return Math.min((Math.abs(distribution.ppf(Math.random())) + 1) * 30000, 1500000)
 }
 
 function shouldSleep() {
@@ -58,17 +69,31 @@ function shouldSleep() {
 }
 
 
+function pickFlower(channelID) {
+    // roll, if < 9. then pick flower
+    if (Math.random(Math.floor((Math.random()*10+1))) < 8) {
+        discordBot.sendMessage({
+            to: channelID,
+            message: ".pick"
+        });
+        console.log('picked flowers');
+    }
+    else {
+        console.log('spared flowers this time');
+    }
+}
+
 // as stated...
 function getFishy() {
     discordBot.sendMessage({
         to: channelID,
         message: "t!fish"
     });
-    
+
     console.log('fished');
-    
+
     var nextInterval = getRandomPause();
-    setTimeout(getFishy, nextInterval);
-    
+    setTimeout(getFishy, nextInterval*5);
+
     console.log("queued self to fire in " + nextInterval + "ms");
 }
