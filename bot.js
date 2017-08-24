2c931f31873b4d5e911d8143a81eeb6cf509c1fc
charuri <charlina.hung@gmail.com>
2017-08-24 00:28:29 -0500

    added delay back in to flower picking

--- bot.js
+++ bot.js
@@ -138,17 +138,17 @@ function getSassy(channelID) {
 
 // crush ppl but optionally be nice sometimes
 function pickFlower(channelID) {
-    var delay = (config.killMode ? 300 : 500);
+    var delay = (config.killMode ? 100 : 500);
     // roll, if < 8. then pick flower
 
     if (config.killMode || (Math.random() < config.pickThreshold)) {
-        // setTimeout(function(){
-        //     // console.log('delay');
-        // }, delay);
-        discordBot.sendMessage({
-            to: channelID,
-            message: ".pick"
-        });
+        setTimeout(function(){
+            // console.log('delay');
+            discordBot.sendMessage({
+                to: channelID,
+                message: ".pick"
+            });
+        }, delay);
         console.log('sniped flowers');
     }
     else {
