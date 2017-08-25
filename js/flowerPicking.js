c5ad7ef67480fbcb4cbe5f1edb3f28ebc4744b45
charuri <charlina.hung@gmail.com>
2017-08-24 22:42:46 -0500

    updated flower picking kill mode threshold

--- js/flowerPicking.js
+++ js/flowerPicking.js
@@ -2,7 +2,7 @@ import { Discord, discordBot, channelID, config } from "../bot.js";
 
 // crush ppl but optionally be nice sometimes
 export default function pickFlower(channelID) {
-    var delay = (config.killMode ? 100 : 500);
+    var delay = (config.killMode ? 150 : 500);
     // roll, if < 8. then pick flower
 
     if (config.killMode || (Math.random() < config.pickThreshold)) {
